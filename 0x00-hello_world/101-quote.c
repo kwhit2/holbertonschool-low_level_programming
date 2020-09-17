d605521abf4c9eeb51985d57d639f44a752b8874
Kyle Whitten <kfw2@outlook.com>
2020-09-17 01:19:57 +0000

    Task 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+/**
+* main- void
+* Description: Prints > and that piece of art is useful" - Dora Korpar, 2015-10-19,
+* followed by a new line, to the standard error.
+* Return:1
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}

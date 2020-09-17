17c52e5e1d4040d2c4dbb5943ce6ebea19e10bde
Kyle Whitten <kfw2@outlook.com>
2020-09-17 17:25:47 +0000

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -24,11 +24,11 @@ int main(void)
 
 	else if (n < 0)
 	{
-		printf("%d is negative\n," n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("%d is zero\n,"n);
+		printf("%d is zero\n", n);
 	}
 
 	return (0);

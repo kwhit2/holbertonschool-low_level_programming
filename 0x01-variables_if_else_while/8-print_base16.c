fa79961d705c3a04eff3fa145f7e936b8b44246f
Kyle Whitten <kfw2@outlook.com>
2020-09-17 20:45:07 +0000

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main- void
+* Description: Prints all the numbers of base 16
+* in lowercase, followed by a new line.
+* Return:0
+*/
+
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; 0 < 0; i++)
+	{
+		putchar(i);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}

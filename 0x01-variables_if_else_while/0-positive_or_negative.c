18c2f27726ebedf841da28af5b5ab679329e3873
Kyle Whitten <kfw2@outlook.com>
2020-09-17 17:15:21 +0000

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
 * main- void
 * Description: Print whether the number stored
@@ -17,16 +18,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	{
 		printf("%d is positive\n", n);
-	}
+
 	else if (n < 0)
-	{
 		printf("%d is negative\n," n);
-	}
+
 	else
-	{
 		printf("%d is zero\n,"n);
-	}
+
 	return (0);
 }

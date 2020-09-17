0490e15a7e93fecf2d9b8913341f99fc8332208e
Kyle Whitten <kfw2@outlook.com>
2020-09-17 01:24:47 +0000

    Task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 /**
 * main- void
-* Description: Prints > and that piece of art is useful" - Dora Korpar, 2015-10-19,
-* followed by a new line, to the standard error.
+* Description: Prints > and that piece of art is useful" - Dora Korpar,
+*2015-10-19, followed by a new line, to the standard error.
 * Return:1
 */
 
@@ -9,7 +9,8 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }

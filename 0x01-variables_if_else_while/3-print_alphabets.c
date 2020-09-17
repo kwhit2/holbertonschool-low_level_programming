a8d975738089acdb501e7ab18fcf163f73b6c86a
Kyle Whitten <kfw2@outlook.com>
2020-09-17 19:40:23 +0000

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main- void
+* Description: Print whether the number stored
+* in the variable n is positive or negative.
+* computer it is compiled and run on.
+* Return:0
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}

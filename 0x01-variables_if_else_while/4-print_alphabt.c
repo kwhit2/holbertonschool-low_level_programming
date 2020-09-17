cf8b487a18b0818a72669b530d042cc6971562e7
Kyle Whitten <kfw2@outlook.com>
2020-09-17 19:51:59 +0000

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main- void
+* Description: Print whether the number stored
+* in the variable n is positive or negative.
+* computer it is compiled and run on.
+* Return:0
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

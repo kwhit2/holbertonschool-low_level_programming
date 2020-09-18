272cf35df06454d149ba04af7098c9bea8e4b208
Kyle Whitten <kfw2@outlook.com>
2020-09-18 01:33:34 +0000

    Task 11

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,13 +15,13 @@ int main(void)
 	{
 		for (d2 = d1 + 1; d2 < 10; d2++)
 		{
+			if (d1 == 8 && d2 == 9)
+			continue;
+
 			putchar((d1 % 10) + '0');
 			putchar((d2 % 10) + '0');
 
-		if (d1 == 8 && d2 == 9)
-			continue;
-
-			putchar('\n');
+			putchar(',');
 			putchar(' ');
 		}
 	}

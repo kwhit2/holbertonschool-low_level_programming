8778a4c1d3a8101021dc8858d3cbdeff49e1abac
Kyle Whitten <kfw2@outlook.com>
2020-09-19 02:18:40 +0000

    Task 12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,25 +11,27 @@ int main(void)
 {
 	int d1, d2, d3;
 
-	for (d1 = 0; d1 < 10; d1++)
+	for (d1 = 0; d1 <= '9'; d1++)
 	{
-		for (d2 = d1 + 1; d2 < 10; d2++)
+		for (d2 = d1 + 1; d2 <= '9'; d2++)
 		{
-			for (d3 = d2 + 1; d3 < 10; d3++)
+			for (d3 = d2 + 1; d3 <= '9'; d3++)
 			{
-			putchar((d1 % 10) + '0');
-			putchar((d2 % 10) + '0');
-			putchar((d3 % 10) + '0');
+			putchar(d1);
+			putchar(d2);
+			putchar(d3);
 
-		if (d1 == 1 && d2 == 0 && d3 == 2)
-			continue;
-
+		if (d1 == '7' && d2 == '8' && d3 == '9')
+		{
+			putchar('\n');
+		}
+		else
+		{
 			putchar(',');
 			putchar(' ');
-			}
 		}
 	}
-	putchar('\n');
-
+ }
+}
 	return (0);
 }

b1bf39063242bd3fc84e418cfbc513dce96fda2f
Kyle Whitten <kfw2@outlook.com>
2020-09-22 14:56:16 +0000

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* add - adds two integers
+* and returns the result.
+* @a: number 1
+* @b: number 2
+* Return:void
+*
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}

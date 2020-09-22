c894ebe3802950ceb74373548c5d377c7e29cccf
Kyle Whitten <kfw2@outlook.com>
2020-09-22 14:17:47 +0000

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,14 @@
 #include "holberton.h"
 
 /**
-* int _isalpha - Function that checks
-* for alphabetic character.
-* Return:0
-*
-*/
+ *int _isalpha - function that checks for
+ * alphabetic character.
+ *@c: Alpha characters
+ *
+ *Description: Returns 1 if c is lowercase, else 0
+ *section header: Section description
+ *Return: 1 if a letter or 0 if not
+ */
 
 int _isalpha(int c)
 {

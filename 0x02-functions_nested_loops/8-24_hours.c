3f745b5f61f24952e7a4f178da21940283835902
Kyle Whitten <kfw2@outlook.com>
2020-09-22 01:40:10 +0000

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,11 +4,24 @@
 * void jack_bauer - Function that prints every
 * minute of the day of Jack Bauer,
 * starting from 00:00 to 23:59.
-* Return:0
+* Return:void
 *
 */
 
 void jack_bauer(void)
 {
+	int d1, d2;
 
+	for (d1 = 00; d1 <= 23; d1++)
+	{
+	for (d2 = 00; d2 <= 59; d2++)
+	{
+	_putchar(d1 / 10 % 10 + '0');
+	_putchar(d1 % 10 + '0');
+	_putchar(':');
+	_putchar(d2 / 10 % 10 + '0');
+	_putchar(d2 % 10 + '0');
+	_putchar('\n');
+	}
+}
 }

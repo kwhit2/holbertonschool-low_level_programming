9ec58c9f2fbd3aba6802d9ddd2e00e00d6bd985a
Kyle Whitten <kfw2@outlook.com>
2020-09-22 13:58:38 +0000

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,18 +3,18 @@
 /**
 * int _islower - Prints the alphabet,
 * 10x in lowercase, followed by a new line.
-* Return:0
+* Return:0 or 1
 *
 */
 
 int _islower(int c)
 {
-    if (c >= 97 && c <= 122)
-    {
-        return (1);
-    }
-        else
-        {
-            return (0);
-        }
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
 }

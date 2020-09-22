8bfe3eb6def7d93a58347676eea4c9c700d5b783
Kyle Whitten <kfw2@outlook.com>
2020-09-22 22:31:59 +0000

    Task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* print_times_table - function that prints the n
+* times table, starting with 0.
+* @n: digit
+* Return:n
+*
+*/
+
+void print_times_table(int n)
+{
+    return (n);
+}

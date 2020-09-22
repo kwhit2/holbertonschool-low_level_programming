60d377ed7f5b9345a936ae62dcf30f063165f193
Kyle Whitten <kfw2@outlook.com>
2020-09-22 22:34:16 +0000

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,5 +10,27 @@
 
 void print_times_table(int n)
 {
+    {
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+	_putchar('0');
+
+	for (b = 1; b <= 9; b++)
+	{
+	_putchar(',');
+	_putchar(' ');
+
+	c = a * b;
+
+	if (c <= 9)
+	_putchar(' ');
+	else
+		_putchar((c / 10) + '0');
+
+	_putchar((c % 10) + '0');
+	}
+	_putchar('\n');
     return (n);
 }

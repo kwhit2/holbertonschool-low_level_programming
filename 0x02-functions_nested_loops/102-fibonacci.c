eabfb129316b2341fa0c9691642c1917736bdc74
Kyle Whitten <kfw2@outlook.com>
2020-09-23 22:15:49 +0000

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+/**
+* main- prints fibonacci numbers
+* @n: number
+* Return:0
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	long int num1 = 0, num2 = 1, i, fib = 0;
+
+	for (i = 1; i <= 50; i++)
+	{
+	printf("%ld, ", num2);
+
+	fib = num1 + num2;
+
+	num1 = num2;
+	num2 = fib;
+
+	}
+
+	return (0);
+}

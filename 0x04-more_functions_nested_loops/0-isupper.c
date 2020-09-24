bbac08ff6866bae85b6ce288e429edb79a56b585
Kyle Whitten <kfw2@outlook.com>
2020-09-24 14:33:51 +0000

    Task 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,10 +4,10 @@
  * _isupper - Checks for Uppercase character.
  *@c: Uppercase A-Z
  *
- *Description: Returns (1) if c is uppercase,
+ *Description: Returns (1) if c is a Uppercase,
  *else return (0)
- *section header: Section description
- *Return: returns int
+ *Return: Returns (1) if c is uppercase,
+ *else return (0)
  */
 
 int _isupper(int c)

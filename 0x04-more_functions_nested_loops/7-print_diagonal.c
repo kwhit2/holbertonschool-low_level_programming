81e4b1041450ed955f26af1d8c9a8edc390a22fc
Kyle Whitten <kfw2@outlook.com>
2020-09-24 19:24:35 +0000

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+* print_diagonal - Draws a diagonal
+* line on the terminal.
+* @n: _
+* Description - Draws a diagonal
+* line on the terminal.
+* Return:void
+*
+*/
+
+void print_diagonal(int n)
+{
+	int ln, spc;
+
+	if (n > 0)
+	{
+	for (ln = 0; ln < n; ln++)
+	{
+	for (spc = 0; spc < ln; spc++)
+		_putchar(' ');
+		_putchar('\\');
+
+		if (ln == n - 1)
+		continue;
+
+	_putchar('\n');
+	}
+    }
+    _putchar('\n');
+}

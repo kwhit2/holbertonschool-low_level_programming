81446414d5cccc658a5078ebb13f07f0b2ecbf6f
Kyle Whitten <kfw2@outlook.com>
2020-09-24 20:30:09 +0000

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,18 +14,18 @@ void print_square(int size)
 {
 	int h, w;
 
-	if (size > 0)
-	{
 	for (h = 0; h < size; h++)
 	{
-	for (w = 0; w < size; w++)
+		for (w = 0; w < size; w++)
+		{
 		_putchar('#');
+		}
 
-		if (h == size - 1)
-		continue;
+		_putchar('\n');
 
+	if (size <= 0)
+	{
 	_putchar('\n');
 	}
 	}
-	_putchar('\n');
 }

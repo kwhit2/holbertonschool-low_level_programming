39202b719642f19b8af979992f7ca4bd983c0c75
Kyle Whitten <kfw2@outlook.com>
2020-09-24 17:40:29 +0000

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* print_line - Draws a straight
+* line in the terminal.
+* @n: _
+* Description - Draws a straight
+* line in the terminal.
+* Return:void
+*
+*/
+
+void print_line(int n)
+{
+	int ln;
+
+	if (n > 0)
+	{
+	for (ln = 0; ln < n; ln++)
+		_putchar('_');
+	}
+	_putchar('\n');
+}

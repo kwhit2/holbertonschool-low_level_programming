37f2da4e03949a36b35dc26092198533a8c360ff
Kyle Whitten <kfw2@outlook.com>
2020-09-29 19:30:14 +0000

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _atoi - Function that convert a string to an integer.
+ * buffer pointed to by dest.
+ * @s: string converted to an interger
+ * Description: Function that convert a string to an integer.
+ * Return: res
+ *
+ */
+
+int _atoi(char *s);
+{
+int res;
+int i;
+res = 0;
+for (i = 0; s[i] != '\0'; i++)
+res = res * 10 + s[i] - '0';
+return res; 
+}

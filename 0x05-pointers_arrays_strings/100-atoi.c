a81a81f5dfde2b0771ed38e847131e3c67c83ad4
Kyle Whitten <kfw2@outlook.com>
2020-09-29 19:32:15 +0000

    Task 10 Update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,10 +11,10 @@
 
 int _atoi(char *s);
 {
-int res;
-int i;
-res = 0;
-for (i = 0; s[i] != '\0'; i++)
-res = res * 10 + s[i] - '0';
-return res; 
+	int res;
+	int i;
+	res = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	res = res * 10 + s[i] - '0';
+	return res; 
 }

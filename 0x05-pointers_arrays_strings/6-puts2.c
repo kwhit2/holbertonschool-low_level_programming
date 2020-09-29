2e970498e084da52d28c3cacf4a1605468d55d72
Kyle Whitten <kfw2@outlook.com>
2020-09-29 15:12:14 +0000

    Task 6 Update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,10 +14,15 @@ void puts2(char *str)
 {
 	int a;
 
-	while (str[a] != '\0')
-		if (a % 2 == 0)
-		_putchar(str[a]);
-		a++;
+	a = 0;
 
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+		{
+		_putchar(str[a]);
+		}
+		a++;
+	}
 	_putchar('\n');
 }

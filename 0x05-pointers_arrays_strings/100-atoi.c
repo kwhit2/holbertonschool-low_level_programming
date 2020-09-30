c1d09e73921ec5cb998a4aef9f7a9487be30523c
Kyle Whitten <kfw2@outlook.com>
2020-09-30 20:05:32 +0000

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,37 +1,32 @@
 #include "holberton.h"
 #include <stdio.h>
-
 /**
- * _atoi - Function that convert a string to an integer.
- * buffer pointed to by dest.
- * @s: string converted to an interger
- * Description: Function that convert a string to an integer.
- * Return: res
+ *_atoi - function that convert a string to an integer.
+ *@s: *s pointer
  *
+ *Description: function that convert a string to an integer.
+ *section header: Section description
+ *Return: returns integer
  */
-
 int _atoi(char *s)
 {
-	short boo;
-	int i, min, res;
-
-	i = min = res = boo = 0;
-	min = -1;
-
-	while (s[i] != '\0')
-	{
-	if (s[i] == '-')
-		min *= -1;
-
-	if (s[i] >= '0' && s[i] <= '9')
-	{
-		res *= 10;
-		res -= (s[i] - '0');
-		boo = 1;
-	}
-	else if (boo == 1)
-	break;
-	i++;
+short boo;
+int i, min, res;
+i = min = res = boo = 0;
+min = -1;
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+min *= -1;
+if (s[i] >= '0' && s[i] <= '9')
+{
+res *= 10;
+res -= (s[i] - '0');
+boo = 1;
+}
+else if (boo == 1)
+break;
+i++;
 }
 res *= min;
 return (res);

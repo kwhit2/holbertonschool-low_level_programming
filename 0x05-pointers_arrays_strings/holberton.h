e35a07717f6a1ee9054b4a5e4bb8c845f9b9d247
Kyle Whitten <kfw2@outlook.com>
2020-09-30 19:55:31 +0000

    holberton.h

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdio.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -13,6 +14,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-#include <stdio.h>
+
 
 #endif

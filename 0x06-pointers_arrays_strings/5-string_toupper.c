b7bb0b484cd6be3e83b92d6f0263a286683b70da
Kyle Whitten <kfw2@outlook.com>
2020-10-02 13:53:05 +0000

    Task 5 Update

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -22,3 +22,14 @@ char *string_toupper(char *s)
 	}
 	return (s);
 }
+/*
+ * Same program with a for loop;
+ * int i;
+ * for (i = 0; s[i] != '\0'; i++)
+ * {
+ * if (s[i] >= 'a' && s[i] <= 'z')
+ * s[i] = s[i] - 32;
+ * }
+ * return (s);
+ * }
+ */

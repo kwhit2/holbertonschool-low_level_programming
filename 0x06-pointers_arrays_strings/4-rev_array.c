24f43d88c3164c947644e7b9ae2a7b6dffef25ca
Kyle Whitten <kfw2@outlook.com>
2020-10-04 22:43:08 +0000

    Comments Optimizing while loop

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,7 +11,7 @@
 
 void reverse_array(int *a, int n)
 {
-	int j = 0, k;
+	int j = 0, k; /* k is a temp variable */
 
 	n -= 1;
 	while (j < n)
@@ -23,3 +23,15 @@ void reverse_array(int *a, int n)
 		n--;
 	}
 }
+/*
+* Optimized while loop below
+*
+*while (j < n)
+*{
+*	k = a[j];
+*	a[j] = a[n];
+*	a[n] = k;
+*	j++;
+*	n--;
+*}
+*/

349766901632878a4da7544523079b87eb8f167c
Kyle Whitten <kfw2@outlook.com>
2020-10-05 19:48:11 +0000

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+* _strstr - Function that locates a substring.
+* @haystack: string where to check for needle
+* @needle: substring to find in haystack
+*
+* Return: a pointer to the beginning of the located
+* substring, or NULL if the substring is not found.
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+	if (needle[j])
+	{
+		i++;
+		j = 0;
+	}
+	else
+	return (haystack + i);
+}
+return (0);
+}

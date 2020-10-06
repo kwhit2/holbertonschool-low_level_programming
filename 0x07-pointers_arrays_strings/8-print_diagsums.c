cd567922af6ac0ebc842304370c64bf99774471d
Kyle Whitten <kfw2@outlook.com>
2020-10-06 20:18:25 +0000

    Updated Code, Comments & Readability

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,18 +9,21 @@
 */
 void print_diagsums(int *a, int size)
 {
-int i, j, k, l, m;
-l = 0;
-m = 0;
-for (i = 0; i < size; i++)
-{
-k = (i * size) + i;
-l += *(a + k);
-}
-for (j = 0; j < size; j++)
-{
-k = (j * size) + (size - 1 - j);
-m += *(a + k);
-}
-printf("%i, %i\n", l, m);
+	int i, j, t1, t2; /*i=rows, j=columns, t1=total1, t2=total2*/
+
+	t1 = 0;
+	t2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+				t1 += *((a + i * size) + j);
+			if (i + j == (size - 1))
+				t2 += *((a + i * size) + j);
+		}
+	}
+
+	printf("%d, %d\n", t1, t2);
 }

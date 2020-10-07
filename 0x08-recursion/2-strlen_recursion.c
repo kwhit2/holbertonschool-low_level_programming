72de35d5a4e3505d1f87542aa19d48c62df3a86b
Kyle Whitten <kfw2@outlook.com>
2020-10-07 16:15:58 +0000

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* _strlen_recursion - Returns the length of a string
+* @s: string to find length of
+*
+* Return: length of string (len)
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (_strlen_recursion(s + 1) + 1);
+
+}

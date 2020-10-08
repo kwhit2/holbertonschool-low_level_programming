0787d067c8c356408ef6fa6de4c4dcbc450644b8
Kyle Whitten <kfw2@outlook.com>
2020-10-08 00:46:03 +0000

    File & Comments Task 8

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* wildcmp -Compares two strings and returns 1 if the
+* strings can be considered identical, otherwise return 0.
+* @s1: string one, has no wildcards
+* @s2: string two, can have a wildcard '*'
+* Return: Returns 1 if the strings can be
+* considered identical, otherwise return 0.
+*/
+int wildcmp(char *s1, char *s2)
+{
+    
+}

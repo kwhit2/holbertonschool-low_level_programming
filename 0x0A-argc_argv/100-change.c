29948d5e989f00c1ef2fa3361926ba8c54643bf7
Kyle Whitten <kfw2@outlook.com>
2020-10-12 21:30:14 +0000

    Advanced Task

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the minimum number of coins to
+ * make change for an amount of money.
+ *
+ * @argc: The number of arguments given to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If the number of arguments is not exactly one return -1.
+ * Otherwise return 0
+*/
+int main(int argc, char *argv[])
+{
+	int coins, total;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	total = atoi(argv[1]);
+
+	if (total < 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (total != 0)
+	{
+	if (total >= 25)
+		total -= 25;
+	else if (total >= 10)
+		total -= 10;
+	else if (total >= 5)
+		total -= 5;
+	else if (total >= 2)
+		total -= 2;
+	else if (total >= 1)
+		total -= 1;
+	coins++;
+	}
+
+	printf("%d\n", coins);
+	return (0);
+}

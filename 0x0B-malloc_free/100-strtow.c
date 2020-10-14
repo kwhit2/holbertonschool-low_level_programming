17e45fbaee385fbc401ee121a97439ff39ef1398
Kyle Whitten <kfw2@outlook.com>
2020-10-14 01:02:32 +0000

    Task 5 & 6 Updates

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* **strtow- Function that splits a string into words.
+* @str: string to be split into words
+*
+* Return: Returns a pointer to an array of strings (words)
+* NULL if str == NULL or str == "" and/or if functions fail return NULL
+*/
+char **strtow(char *str)
+{
+	if (str == NULL || str == "")
+		return (NULL);
+}
--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -2,17 +2,14 @@
 #include <stdlib.h>
 /**
 * *argstostr- Function that concatenates all the arguments of your program.
-* @ac: int
-* @av: double pointer
+* @ac: Number of arguments passed into the program
+* @av: Array of pointers to the arguments
 *
 * Return: NULL if ac == 0 or av == NULL and/or
 * a pointer to a new string, or NULL if it fails.
 */
 char *argstostr(int ac, char **av)
 {
-	if (ac == 0)
-		return (NULL);
-
-	if (av == NULL)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 }

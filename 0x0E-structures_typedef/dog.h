89de27ee6a17de5bb75ca65ae9bd0a5c1ba4b6f5
Kyle Whitten <kfw2@outlook.com>
2020-10-19 18:11:23 +0000

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+/**
+ * struct dog- Struct with name, age & owner.
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: Struct with name, age & owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

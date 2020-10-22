136d2f420bfbd6f3561abdc62e82ff96258a6d17
Kyle Whitten <kfw2@outlook.com>
2020-10-22 02:26:02 +0000

    Task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/**
+* main- Prints op codes of its main function.
+*
+* @argc: argument count
+* @argv: argument vector array
+*
+* Return: 0
+*/
+int main(int argc, char **argv)
+{
+if (argc != 2)
+{
+	printf("Error\n");
+	exit(1);
+}
+
+if (atoi(argv[1]) < 0)
+{
+	printf("Error\n");
+	exit(2);
+}
+
+return (0);
+}

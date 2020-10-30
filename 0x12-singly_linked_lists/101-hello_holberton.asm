4d4db42f2ca6e3307483c143c6adbe4cbb14b740
kwhit2 <kfw2@outlook.com>
2020-10-30 14:20:57 +0000

    Updated Task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,25 @@
-	extern printf
+ 	extern printf
+
 
 	section .data
 
-msg:	db "Hello, Holberton", 0
-fmt:	db "%s", 10, 0
+msg:	db "Hello, Holberton", 0	; C string needs 0
+fmt:	db "%s", 10, 0		; The printf format, "\n", '0'
 
-	section .text
+	section .text		; Code section.
 
 	global main
 main:
 	push	rbp
 
-	mov	rdi,fmt
-	mov	rdi,msg
-	mov	rax,0
-	call	printf
 
-	pop	rbp
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0		; or can be    xor   rax,rax
+	call	printf		; Call C function
 
-	mov	rax,0
+	pop rbp		; restore stack
+
+
+	mov rax,0		; normal, no error, return value
 	ret

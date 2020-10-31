9b6ea87afb5778a9f7a047d000f108948b7292be
kwhit2 <kfw2@outlook.com>
2020-10-31 03:00:48 +0000

    Updated Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,16 +14,12 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] %p\n", h->str);
-		}
 
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-			nodecount++;
-		}
+	nodecount++;
+	h = h->next;
 	}
 return (nodecount);
 }

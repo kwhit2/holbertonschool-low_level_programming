1b48a5033ae330323b1d7c4565825179c00eedcb
kwhit2 <kfw2@outlook.com>
2020-11-03 16:46:36 +0000

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_listint2 - Function that frees a listint_t list.
+* @head: pointer
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+	listint_t *prev = *head;
+
+	while (prev != NULL)
+	{
+		tmp = prev->next;
+		free(prev);
+		prev = tmp;
+	}
+*head = NULL;
+}

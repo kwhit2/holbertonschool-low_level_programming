37903d40387416bb3bddb912469ad202460975f5
kwhit2 <kfw2@outlook.com>
2020-11-05 20:00:59 +0000

    Outline of Task 1:No luck yet

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* print_binary - Function that prints the binary representation of a number.
+* @n: 
+*
+* Return: the converted number, or 0 if there is one or more chars in the
+* string b that is not 0 or 1 and/or b is NULL.
+*/
+
+void print_binary(unsigned long int n)
+{
+
+}

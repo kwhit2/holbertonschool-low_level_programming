70d75eecdac41bfe9a4356a4a32cbad34e0fb1d3
kwhit2 <kfw2@outlook.com>
2020-11-05 16:35:54 +0000

    Task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* clear_bit - Function that sets the value of a bit to 0 at a given index.
+* @index: index of bits to clear
+* @n: int
+* Return: 1 if it worked, or -1 if an error occurred
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned int mask;
+
+	if (index > 32)
+		return (-1);
+	mask = 1 << index;
+	*n &= ~(mask);
+
+return (1);
+}

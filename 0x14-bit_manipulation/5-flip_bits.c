83c303e98bed9f3056797605e05ddb6919c8fe3c
kwhit2 <kfw2@outlook.com>
2020-11-05 20:01:44 +0000

    Set up for Task 5:Returning to it later

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* flip_bits - Function that returns the number of bits you would
+* need to flip to get from one number to another.
+* @n: number 1
+* @m: number 2
+* Return: the number of bits you would need to flip to get from one number to another.
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+}

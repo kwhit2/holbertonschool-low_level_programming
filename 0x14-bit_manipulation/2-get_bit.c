9095c4c333f475bbc881172a7238c93e72190fa8
kwhit2 <kfw2@outlook.com>
2020-11-05 16:26:59 +0000

    Task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* get_bit - Function that returns the value of a bit at a given index.
+* @index: index of bits
+* @n: int
+* Return: the value of the bit at index index or -1 if an error occured.
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned int mask;
+
+	if (index > 32)
+		return (-1);
+	mask = 1 << index;
+    mask = mask & n;
+    mask = mask >> index;
+
+return (mask);
+}

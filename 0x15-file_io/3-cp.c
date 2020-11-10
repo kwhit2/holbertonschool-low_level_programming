aebc0c10609a2dd817a377a036eb9424cf57c578
kwhit2 <kfw2@outlook.com>
2020-11-10 19:42:30 +0000

    Task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* main -  program that copies the content of a file to another file.
+* @argc: int count of arguments
+* @argv: char double pointer
+* Return: int
+*/
+
+int main(int argc, char **argv)
+{
+int rd, wr, fd1, fd2, cl2, cl2;
+char buff[1024];
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+return (0);
+}

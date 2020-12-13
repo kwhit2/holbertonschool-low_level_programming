dcf160a89e1414f71d6885b3eb2757b32d268e87
kwhit2 <kfw2@outlook.com>
2020-12-13 16:39:40 +0000

    Fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newnode->next = *head;
 	newnode->prev = NULL;
 
-	if (head != NULL)
+	if ((*head) != NULL)
 	(*head)->prev = NULL;
 
 	(*head) = newnode;

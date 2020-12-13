6277f1c10b6d78ee59e2d43cfa702e92cdc24717
kwhit2 <kfw2@outlook.com>
2020-12-13 18:33:32 +0000

    Betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -45,7 +45,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 return (NULL);
 }
 
-/*\
+/*
 * old - may need for reference later
 * new->n = n;
 * new->next = cpy->next;

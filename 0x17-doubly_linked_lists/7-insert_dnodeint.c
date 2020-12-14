17f392900baccab6f6605169a397682c8bb1fcea
kwhit2 <kfw2@outlook.com>
2020-12-14 21:01:26 +0000

    Comments

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -46,9 +46,20 @@ return (NULL);
 }
 
 /*
-* old - may need for reference later
-* new->n = n;
-* new->next = cpy->next;
-* cpy->next = new;
-* return (new);
+* declare a copy of list (cpy = *h), and 'new' which will be the new node
+* (if the list is empty or there is no list, return NULL)
+* if idx = 0, use function we made to add node at beginning of list
+* for loop: while the list (cpy) exists, AND i is less than idx,
+* move copy along to the next position in the list
+*
+* Now, at the position one before idx, if cpy == NULL and i == idx,
+* use function we made to add node at end of list, using h and n passed to us
+* Else if cpy at the position before idx is NOT NULL,
+* 1. malloc space for the size of the new node
+* 2. check if malloc worked
+* 3. set the data of new to contain n, next, and prev from cpy
+* 4. set pointer 'next' of new to point to cpy (new->next = cpy);
+* 5. return address of new node
+* Otherwise, return NULL because it failed
 */
+

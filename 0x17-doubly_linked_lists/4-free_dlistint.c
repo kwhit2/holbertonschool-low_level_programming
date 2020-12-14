3720683aa3d619d27acd8088a4e71082f3f39d19
kwhit2 <kfw2@outlook.com>
2020-12-14 20:23:49 +0000

    Comments

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,12 +7,21 @@
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
-	dlistint_t *prev = head;
+	dlistint_t *copy = head;
 
-	while (prev != NULL)
+	while (copy != NULL)
 	{
-		tmp = prev->next;
-		free(prev);
-		prev = tmp;
+		tmp = copy->next;
+		free(copy);
+		copy = tmp;
 	}
 }
+
+/*
+* declare pointer to tmp
+* declare pointer to copy = head to make a copy of the list
+* while the list exists
+* 1. set tmp equal to 1 node past copy
+* 2. free copy (1st time through this is head)
+* 3. move copy to 1 down the list into the position tmp was holding
+*/

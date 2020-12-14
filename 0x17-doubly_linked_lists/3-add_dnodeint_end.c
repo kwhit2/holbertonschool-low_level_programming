3725b9179a827e293fd558ef671fd0e7c83e1892
kwhit2 <kfw2@outlook.com>
2020-12-14 20:09:53 +0000

    Comments

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,15 +13,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *newnode;
 	dlistint_t *lastnode;
 
-	if (head == NULL)
-		return (NULL);
-
 	newnode = (dlistint_t *) malloc(sizeof(dlistint_t));
 
 	if (newnode == NULL)
 		return (NULL);
 
-	lastnode = (*head); /* see comments below */
+	lastnode = (*head);
 
 	newnode->next = NULL;
 	newnode->n = n;
@@ -29,10 +26,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if ((*head) == NULL)
 	{
 	newnode->prev = NULL;
-	(*head) = newnode; /*parens not needed around *head */
-		return ((newnode)); /*but maybe it adds to reability?*/
+	(*head) = newnode;
+		return ((newnode));
 	}
-	lastnode = (*head);
 
 	while (lastnode->next != NULL)
 		lastnode = lastnode->next;
@@ -40,5 +36,21 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	lastnode->next = newnode;
 	newnode->prev = lastnode;
 
-return (lastnode);
+return (newnode);
 }
+
+/*
+* Lines 13-2: Declare pointer to lastnode set it equal to head(it's a copy of
+* the list).
+* Lines 13-21: Declare pointer to newnode, malloc for it and check if malloc
+* worked.
+* Line 23: set next pointer of newnode to point to NULL
+* Line 24: copy n that is passed into function into node
+* Line 26: If there is no list yet set prev pointer of newnode to point to NULL
+* set head to be newnode.
+* Line 30: return newnode
+* Line 33: while is to get the position of lastnode to one before end of list.
+* Line 36: At the end of the list so you set the lastnode to be the newnode.
+* Line 37: Set prev pointer of newnode to point to lastnode.
+* Return address of newnode.
+*/
